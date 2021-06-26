73e90ff70dc2c8a2c58151485e32c02fafe0caa3
Jake Millhiser <jake@Jakes-MacBook-Air.local>
2021-06-25 20:27:17 -0700

    In Progress: Working on porting training step.

---
+++ CNN_Model.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+# project image min and max values to 0 and 1
+def project_01(im):
+    im = np.squeeze(im)
+    min_val = im.min()
+    max_val = im.max()
+    return (im - min_val) / (max_val - min_val)
+
+# normalize image by given mean and std
+def normalize_im(im, dmean, dstd):
+    im = np.squeeze(im)
+    im_norm = np.zeros(im.shape, dtype=np.float32)
+    im_norm = (im - dmean)/dstd
+    return im_norm
+
+# define 2D gaussian filter function to mimic 
+# MATLAB's fspecial('gaussian', [shape], [sigma])
+def matlab_style_gauss2D(shape=(7,7), sigma=1):
+    m, n = [(ss - 1.0) / 2.0 for ss in shape]
+    y, x = np.ogrid[-m:m+1, -n:n+1]
+    h = np.exp(-(x**2 + y**2) / (2.0*sigma**2))
+    h.astype('float32')
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    h = h * 2.0
+    h = h.astype('float32')
+    return h
+
+# prepare the filter used in Deep-STORM
+psf_heatmap = torch.tensor(matlab_style_gauss2D(shape=(7, 7), sigma=1))
+gfilter = torch.reshape(psf_heatmap, [1, 1, 7, 7])
+
+# Deep-STORM Loss function
+def l1l2loss(heatmap_true, spikes_pred):
+    # use spikes to create heatmaps
+    heatmap_pred = F.conv2d(spikes_pred, gfilter, padding=3) # why same isn't working
+    # heatmap l2 error
+    loss_heatmap = F.mse_loss(heatmap_true, heatmap_pred)
+    # spikes l1 error
+    loss_spikes = F.l1_loss(spikes_pred, torch.zeros_like(spikes_pred))
+    return loss_heatmap + loss_spikes
+
+class DeepSTORM(nn.Module):
+    def __init__(self, input_shape):
+        super(DeepSTORM, self).__init__()
+
+        # "Encoder" Part of Network
+        self.features1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        self.pool1 = nn.MaxPool2d(2)
+        self.features2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.pool2 = nn.MaxPool2d(2)
+        self.features3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        self.pool3 = nn.MaxPool2d(2)
+        self.features4 = nn.Sequential(
+            nn.Conv2d(128, 512, 3, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU()
+        )
+
+        # "Decoder" Part of Network
+        self.up5 = nn.Upsample(scale_factor=2)
+        self.features5 = nn.Sequential(
+            nn.Conv2d(512, 128, 3, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        self.up6 = nn.Upsample(scale_factor=2)
+        self.features6 = nn.Sequential(
+            nn.Conv2d(128, 64, 3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.up7 = nn.Upsample(scale_factor=2)
+        self.features7 = nn.Sequential(
+            nn.Conv2d(64, 32, 3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        self.prediction = nn.Conv2d(32, 1, 1, padding=0, bias=False)
+
+    def forward(self, x):
+        x = self.features1(x)
+        x = self.pool1(x)
+        x = self.features2(x)
+        x = self.pool2(x)
+        x = self.features3(x)
+        x = self.pool3(x)
+        x = self.features4(x)
+        x = self.up5(x)
+        x = self.features5(x)
+        x = self.up6(x)
+        x = self.features6(x)
+        x = self.up7(x)
+        x = self.features7(x)
+        y = self.prediction(x)
+        return y
+
+if __name__ == "__main__":
+    network = DeepSTORM((16,16))
+    print(network)
---
+++ Training.py
@@ -0,0 +1,134 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy.io as sio
+import argparse
+from os.path import abspath
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+from CNN_Model import project_01, normalize_im, DeepSTORM, l1l2loss
+
+device = ("cuda" if torch.cuda.is_available() else "cpu") 
+
+class SimpleSTORMDataset(Dataset):
+    def __init__(self, trainX, trainY, transform=None, target_transform=None):
+        self.img_labels = trainY
+        self.img_points = trainX
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return self.img_labels.shape[0]
+
+    def __getitem__(self, idx):
+        return (self.img_points[idx], self.img_labels[idx])
+
+def train_model(filename, weights_name, meanstd_name):
+    # for reproducibility
+    np.random.seed(123)
+
+    # load the datasets
+    matfile = h5py.File(filename, 'r')
+    patches = np.array(matfile['patches'])
+    heatmaps = 100.0 * np.array(matfile['heatmaps'])
+
+    # split dataset into training and validation sets
+    trainX, testX, trainY, testY = train_test_split(patches, heatmaps, \
+            test_size=0.3,
+            random_state=42)
+    print(f'Number of Training Examples: {trainX.shape[0]}')
+    print(f'Number of Validation Examples: {testX.shape[0]}')
+
+    # ensure all data is float32
+    trainX = trainX.astype('float32')
+    trainY = trainY.astype('float32')
+    testX = testX.astype('float32')
+    testY = testY.astype('float32')
+
+    ## Training Set Normalization ##
+    mean_train = np.zeros(trainX.shape[0], dtype=np.float32)
+    std_train = np.zeros(trainX.shape[0], dtype=np.float32)
+    for i in range(trainX.shape[0]):
+        trainX[i, ...] = project_01(trainX[i, ...])
+        mean_train[i] = trainX[i, ...].mean()
+        std_train[i] = trainX[i, ...].std()
+    mean_val_train = mean_train.mean()
+    std_val_train = std_train.mean()
+    trainX_norm = np.zeros(trainX.shape, dtype=np.float32)
+    for i in range(trainX.shape[0]):
+        trainX_norm[i, ...] = normalize_im(trainX[i, ...], \
+                mean_val_train, std_val_train)
+    # patch size
+    psize = trainX_norm.shape[1]
+    trainX_norm = trainX_norm.reshape(trainX.shape[0], 1, psize, psize)
+
+    ## Test Set Normalization ##
+    mean_test = np.zeros(testX.shape[0], dtype=np.float32)
+    std_test = np.zeros(testX.shape[0], dtype=np.float32)
+    for i in range(testX.shape[0]):
+        testX[i, ...] = project_01(testX[i, ...])
+        mean_test[i] = testX[i, ...].mean()
+        std_test[i] = testX[i, ...].std()
+    mean_val_test = mean_test.mean()
+    std_val_test = std_test.mean()
+    testX_norm = np.zeros(testX.shape, dtype=np.float32)
+    for i in range(testX.shape[0]):
+        testX_norm[i, ...] = normalize_im(testX[i, ...], \
+                mean_val_test, std_val_test)
+    testX_norm = testX_norm.reshape(testX.shape[0], 1, psize, psize)
+
+    # label reshaping
+    trainY = trainY.reshape(trainY.shape[0], 1, psize, psize)
+    testY = testY.reshape(testY.shape[0], 1, psize, psize)
+
+    # put data into PyTorch dataset
+    train_dataset = SimpleSTORMDataset(trainX_norm, trainY)
+    train_loader = DataLoader(train_dataset, \
+            batch_size=16, shuffle=True)
+    test_dataset = SimpleSTORMDataset(testX_norm, testY)
+    test_loader = DataLoader(test_dataset, \
+            batch_size=16, shuffle=True)
+
+    # create Deep-STORM model
+    model = DeepSTORM((psize, psize))
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_fn = l1l2loss
+
+    # begin training the model
+    model.train()
+    num_epochs = 100
+    for epoch in range(num_epochs):
+        loop = tqdm(train_loader, total=len(train_loader), leave=True)
+        for data, label in loop:
+            data.to(device)
+            spikes_pred = model(data)
+            loss = loss_fn(label, spikes_pred)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            loop.set_description(f"Epoch [{epoch}/{num_epochs}]")
+            loop.set_postfix(loss = loss.item())
+    return
+
+
+if __name__ == "__main__":
+
+    # parse the required input arguements
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', type=str, \
+            help="path to generated training data m-file")
+    parser.add_argument('--weights_name', type=str, \
+            help="path to save model weights as hdf5-file")
+    parser.add_argument('--meanstd_name', type=str, \
+            help="path to save normalization factors as m-file")
+    args = parser.parse_args()
+
+    # train with given arguements
+    train_model(abspath(args.filename), \
+            abspath(args.weights_name), \
+            abspath(args.meanstd_name))
